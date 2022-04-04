3f5829cb45924d4cf9b51fdbc9e3265f1b31b11a
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-04-03 21:39:19 -0500

    Tipo de variables

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,39 @@
 
 //Declaración de variables
 let firstName = "Carlos"
+
+//Mostrar en console
+console.log(firstName)
+
+//Usar variables descriptivas
+let myFirstJob = 'Developer';
+let myCurrentJob = 'teacher';
+
+//Tipos de datos en javascript
+let myAge = 23;
+let myName = "CarlosDnl";
+let isDeveloper = true;
+let isUndefined;
+//tipo de una variables
+console.log(typeof true);
+console.log(typeof 24);
+console.log(typeof "developer");
+console.log(typeof isUndefined);
+
+//Reasignando el valor de una variable
+isUndefined = false;
+console.log(typeof isUndefined);
+
+/*Formas de declarar variables en javascript
+let = Variables que puedan cambiar más tarde
+const = Variables que no cambiarán
+var = Forma antigua para declarar variables
+*/
+let age = 30;
+age = 32;
+
+const birthYear = 1995;
+var job = 'programmer';
+job = 'teacher';
+
+
