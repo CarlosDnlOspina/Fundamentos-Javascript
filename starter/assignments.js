9c56b134fd8233407f11da7113dae07f66b1b0bb
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-04-04 15:08:16 -0500

    Ejercicio de masa corportal

--- starter/assignments.js
+++ starter/assignments.js
@@ -6,3 +6,16 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+//Ejercicio de masa corporal
+let markMass = 78;
+let markHeigth = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let markBMI = markMass / (markHeigth ** 2);
+let jonhBMI = johnMass / (johnHeight ** 2);
+
+console.log(markBMI, jonhBMI);
+
+let markHigherBMI = markBMI > jonhBMI;
