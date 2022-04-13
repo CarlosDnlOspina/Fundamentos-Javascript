ae451eaabfaaa1573df13521f2d0da8c81a8d64d
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-04-13 10:53:12 -0500

    Ejercicios con funciones

--- starter/assignments.js
+++ starter/assignments.js
@@ -1,48 +1,18 @@
-let country = 'Colombia';
-let continent = 'America';
-let population = 46000000
 
-console.log(country);
-console.log(continent);
-console.log(population);
+const calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
 
-//Ejercicio de masa corporal
-let markMass = 78;
-let markHeigth = 1.69;
-
-let johnMass = 92;
-let johnHeight = 1.95;
-
-let markBMI = markMass / (markHeigth ** 2);
-let jonhBMI = johnMass / (johnHeight ** 2);
-
-console.log(markBMI, jonhBMI);
-
-let markHigherBMI = markBMI > jonhBMI;
-
-if (markBMI > jonhBMI) {
-    console.log(`la masa corporal de Mark es 
-    ${markBMI} 😆`);
-} else {
-    console.log(`La masa corporal de John es ${jonhBMI} 🤣`);
-}
-const dolphinsScore = (96 + 108 + 89) / 3;
-const koalasScore = (88 + 91 + 110) / 3;
-const maxPoint = 100;
-
-if (dolphinsScore > koalasScore && dolphinsScore >= maxPoint) {
-    console.log(`El ganador es Dolphin con ${dolphinsScore}`);
-} else if (dolphinsScore < koalasScore && koalasScore >= maxPoint) {
-    console.log(`El ganador es Koala con ${koalasScore}`)
-} else if (dolphinsScore === koalasScore && dolphinsScore >= maxPoint & koalasScore >= maxPoint) {
-    console.log("Es un empate");
-} else {
-    console.log("Aún no llegan a los 100 puntos");
+function checkkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        return `Dolphin win (${avgDolphins} vs ${avgKoalas})`
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        return `Koalas win (${avgKoalas} vs ${avgDolphins})`
+    } else {
+        return `We not have a winner`;
+    }
 }
 
-//Calculadora de propinas
-let bill = 275;
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
 
-let tip = bill >= 50 && bill <= 300 ? 15 : 20;
-console.log(`El valor de la factura es ${bill} con un propina de ${(tip * bill) / 100} y el total a pagar es de ${bill + ((tip * bill) / 100)}`)
+console.log(checkkWinner(avgDolphins, avgKoalas));
 
